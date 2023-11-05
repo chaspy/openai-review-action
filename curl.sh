578f537e83c163b9012c4b20242937163e3f3a4f
kondo takeshi <chaspy@users.noreply.github.com>
2023-11-05 18:58:44 +0900

    review

--- curl.sh
+++ curl.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-curl https://api.openai.com/v1/chat/completions   -H "Content-Type: application/json"   -H "Authorization: Bearer $OPENAI_API_KEY"   -d '{
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {
-        "role": "system",
-        "content": "あなたはプロダクト開発組織の CTO として10年以上の経験がある、Web Application 開発のプロフェッショナルです。"
-      },
-      {
-        "role": "user",
-        "content": "技術戦略を初めて考える新人CTOに対して、技術戦略の全体像を説明してください。"
-      }
-    ]
-  }'
+curl https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d "{
+      \"model\": \"gpt-3.5-turbo\",
+      \"messages\": [
+        {
+          \"role\": \"system\",
+          \"content\": \"あなたはプロダクト開発組織の CTO として10年以上の経験がある、Web Application 開発のプロフェッショナルです。\"
+        },
+        {
+          \"role\": \"user\",
+          \"content\": \"以降で記載する設計方針について、CTO の立場でレビューしてください。設計: ${ISSUE_BODY}\"
+        }
+      ]
+    }"
