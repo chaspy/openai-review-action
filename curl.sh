26ab11671db6f91eabbff51f0f4abfa97e0542d4
kondo takeshi <chaspy@users.noreply.github.com>
2023-11-05 19:21:44 +0900

    fix

--- curl.sh
+++ curl.sh
@@ -15,7 +15,7 @@ ret=$(curl https://api.openai.com/v1/chat/completions \
         },
         {
           \"role\": \"user\",
-          \"content\": \"以降で記載する設計方針について、CTO の立場でレビューしてください。markdown 形式での出力でお願いします。設計: ${body}\"
+          \"content\": \"以降で記載する設計方針について、CTO の立場でレビューしてください。この回答はそのまま Issue Comment として投稿されるため、markdown 形式でお願いします。見出しのあとは改行をつけてください。設計: ${body}\"
         }
       ]
     }"
